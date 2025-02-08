d2bea60fbe4353b8e691dfc49042c52f02bd1a19
mgdemel <mgdemel@gmail.com>
2025-02-08 10:06:20 +0200

    disable sus link tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin=""
-        />
+        /> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
