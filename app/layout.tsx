b143e7964fadfc40c5107597f96fcd141ba92103
mgdemel <mgdemel@gmail.com>
2025-02-08 11:41:22 +0200

    remove unneeded link to leaflet stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        /> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
